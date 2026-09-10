8855e12db549d8208dc788bb9dd25c6ba23d060e
sky <sky@YHRT.com>
2026-09-10 17:30:28 +0800

    1111

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ try:
     LabelBase.register(
         name='ChineseFont',
         fn_regular= 'NotoSerifCJKsc-Regular.otf'#'NotoSansSC-Regular.otf'
-    )NotoSerifCJKsc-Regular.otf
+    )
     write_log("中文字体注册成功")
 except Exception as e:
     write_log(f"中文字体注册失败: {e}")
