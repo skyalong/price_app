8f8511c1684377496dd571b390ee67232ffea291
sky <sky@YHRT.com>
2026-09-17 09:15:05 +0800

    注释掉主应用中数据库初始化和KV调用

--- main.py
+++ main.py
@@ -1085,8 +1085,8 @@ class PriceApp(MDApp):
             "Button": ["NotoSansCJK", 14, True, 1.25],
         })
         
-        init_db()
-        return Builder.load_string(KV)
+        #init_db()
+        #return Builder.load_string(KV)
 
 
 if __name__ == "__main__":
