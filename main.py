d71bbac8162dc5246af03292bfe23877c8101291
sky <sky@YHRT.com>
2026-09-22 11:15:47 +0800

    PrinceApp

--- main.py
+++ main.py
@@ -94,18 +94,18 @@ class RootWidget(BoxLayout):
 #class PriceApp(MDApp):
 class PriceApp(App):
     def build(self):
-        self.theme_cls.primary_palette = "Blue"
-        self.theme_cls.theme_style = "Light"
+        # self.theme_cls.primary_palette = "Blue"
+        # self.theme_cls.theme_style = "Light"
         
-        # 设置字体
-        self.theme_cls.font_styles.update({
-            "H4": ["NotoSansCJK", 34, False, 0.25],
-            "H5": ["NotoSansCJK", 24, False, 0],
-            "H6": ["NotoSansCJK", 20, False, 0.15],
-            "Subtitle1": ["NotoSansCJK", 16, False, 0.15],
-            "Body1": ["NotoSansCJK", 16, False, 0.5],
-            "Button": ["NotoSansCJK", 14, True, 1.25],
-        })
+        # # 设置字体
+        # self.theme_cls.font_styles.update({
+        #     "H4": ["NotoSansCJK", 34, False, 0.25],
+        #     "H5": ["NotoSansCJK", 24, False, 0],
+        #     "H6": ["NotoSansCJK", 20, False, 0.15],
+        #     "Subtitle1": ["NotoSansCJK", 16, False, 0.15],
+        #     "Body1": ["NotoSansCJK", 16, False, 0.5],
+        #     "Button": ["NotoSansCJK", 14, True, 1.25],
+        # })
         
         #init_db()
         return Builder.load_string(KV)
